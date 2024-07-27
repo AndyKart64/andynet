1576117d6cad83a4de964fe2b9d7fc7347538e2f
Kenson <kenson.hui22@gmail.com>
2024-07-27 17:11:46 -0400

    Add helper functions

--- example.py
+++ example.py
@@ -1,16 +1,28 @@
 #!/bin/python
+import random
+import os
 import torch
 import gym, gym_mupen64plus
 import numpy as np
-import os
-from PIL import Image
 import matplotlib.image
+from PIL import Image
+from collections import deque
 
 def rgb_to_gray(rgb):
+    """
+    Converts the three channel RGB colour to grayscale
+        - rgb : np.ndarray
+    """
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
-from collections import deque
-import random
+def save_grayscale_image(gray, file_name):
+    """
+    Saves the grayscale image to the logs file
+        - gray: np.ndarray
+        - file_name: str
+    """
+    matplotlib.image.imsave('/src/gym_mupen64plus/logs/' + "file_name", gray, cmap='gray')
+
 
 ## Hyperparameters
 ERB_CAPACITY=1000
@@ -49,11 +61,7 @@ for episode in range(EPISODES):
     while True:
         print("Episode " + str(episode) + " =========")
         print("NOOP waiting for green light")
-        
-        ## Sanity Grayscale
-        (obs, rew, end, info) = env.step([0, 0, 1, 0, 0]) # Drive straight
-        obs_grayscale = rgb_to_gray(obs)
-        matplotlib.image.imsave('/src/gym_mupen64plus/logs/sanity.jpeg', obs_grayscale, cmap='gray')
+       
         for i in range(18):
             (obs, rew, end, info) = env.step([0, 0, 0, 0, 0]) # NOOP until green light
 
@@ -68,11 +76,10 @@ for episode in range(EPISODES):
 
             # preprocess image
             # convert observation to greyscale
-            greyscale = np.dot(obs[..., :3], [0.2989, 0.5870, 0.1140])
+            greyscale = rgb_to_gray(obs)
 
             if i == 0:
-                # plt.imsave('saved_greyscale_image.png', greyscale, cmap='gray')
-                pass
+                save_grayscale_image(greyscale, 'saved_greyscale_image.jpg')
 
             # store observation in ERB
 
